3f6e25d8e34ba953314f7025062807edf52eaa49
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-10-21 01:06:11 +0200

    ChessboardSinglePointMutation: fixed call to getter and setter 'encoding' methods instead of 'coding'

--- ga/eight-queens/src/main/java/it/unisa/ga/operator/mutation/ChessboardSinglePointMutation.java
+++ ga/eight-queens/src/main/java/it/unisa/ga/operator/mutation/ChessboardSinglePointMutation.java
@@ -8,12 +8,12 @@ public class ChessboardSinglePointMutation extends RandomResettingMutation<Chess
     
     @Override
     protected ChessboardIndividual mutate(ChessboardIndividual individual, Random rand) throws CloneNotSupportedException {
-        int[] coding = individual.getCoding();
+        int[] coding = individual.getEncoding();
         int col = rand.nextInt(coding.length);
         int row = rand.nextInt(individual.getMaxRows());
         coding[col] = row;
         ChessboardIndividual mutatedIndividual = (ChessboardIndividual) individual.clone();
-        mutatedIndividual.setCoding(coding);
+        mutatedIndividual.setEncoding(coding);
         return mutatedIndividual;
     }
 }

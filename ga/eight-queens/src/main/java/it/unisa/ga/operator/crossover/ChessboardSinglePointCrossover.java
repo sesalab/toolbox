3cf71c04b952c3eb9f117d6b3dcd9f3364131a10
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-10-21 02:57:47 +0200

    ChessboardSinglePointCrossover: apply and cross override method implemented by separating old apply method and constructor added

--- ga/eight-queens/src/main/java/it/unisa/ga/operator/crossover/ChessboardSinglePointCrossover.java
+++ ga/eight-queens/src/main/java/it/unisa/ga/operator/crossover/ChessboardSinglePointCrossover.java
@@ -8,40 +8,51 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Stream;
 
-public class ChessboardSinglePointCrossover extends CrossoverOperator<ChessboardIndividual> {
+public class ChessboardSinglePointCrossover<T extends ChessboardIndividual> extends CrossoverOperator<ChessboardIndividual> {
+
+    public ChessboardSinglePointCrossover(double crossoverProbability, Random random) {
+        super(crossoverProbability, random);
+    }
 
     @Override
-    public Population<ChessboardIndividual> apply(Population<ChessboardIndividual> population, Random rand) throws CloneNotSupportedException {
+    public Population<ChessboardIndividual> apply(Population<ChessboardIndividual> population) throws CloneNotSupportedException {
         Population<ChessboardIndividual> offsprings = population.clone();
         offsprings.setId(population.getId() + 1);
         offsprings.clear();
-
         // Cross parents
         List<Pairing> pairings = makeRandomPairings(population);
         for (Pairing pairing : pairings) {
-            int[] firstCoding = pairing.firstParent.getCoding();
-            int[] secondCoding = pairing.secondParent.getCoding();
-            int minLength = Math.min(firstCoding.length, secondCoding.length);
-            // e.g. if cutPoint = 1, then the cut occurs between 0 and 1, if cutpoint = 2, it occurs between 1 and 2, and so on...
-            int cutPoint = rand.nextInt(minLength - 1) + 1;
-            int[] firstCodingLeft = Arrays.copyOfRange(firstCoding, 0, cutPoint);
-            int[] firstCodingRight = Arrays.copyOfRange(firstCoding, cutPoint, minLength);
-            int[] secondCodingLeft = Arrays.copyOfRange(secondCoding, 0, cutPoint);
-            int[] secondCodingRight = Arrays.copyOfRange(secondCoding, cutPoint, minLength);
-
-            ChessboardIndividual offspring1 = (ChessboardIndividual) pairing.firstParent.clone();
-            offspring1.setCoding(Stream.of(firstCodingLeft, secondCodingRight)
-                    .flatMapToInt(Arrays::stream)
-                    .toArray()
-            );
-            ChessboardIndividual offspring2 = (ChessboardIndividual) pairing.secondParent.clone();
-            offspring2.setCoding(Stream.of(secondCodingLeft, firstCodingRight)
-                    .flatMapToInt(Arrays::stream)
-                    .toArray()
-            );
-            offsprings.add(offspring1);
-            offsprings.add(offspring2);
+            Pairing children = cross(pairing);
+            offsprings.add(children.firstIndividual);
+            offsprings.add(children.secondIndividual);
         }
         return offsprings;
+    }
+
+    @Override
+    protected CrossoverOperator<ChessboardIndividual>.Pairing cross(CrossoverOperator<ChessboardIndividual>.Pairing pairing) throws CloneNotSupportedException {
+
+        int[] firstCoding = pairing.firstIndividual.getEncoding();
+        int[] secondCoding = pairing.secondIndividual.getEncoding();
+        int minLength = Math.min(firstCoding.length, secondCoding.length);
+
+        // e.g. if cutPoint = 1, then the cut occurs between 0 and 1, if cutpoint = 2, it occurs between 1 and 2, and so on...
+        int cutPoint = getRandom().nextInt(minLength - 1) + 1;
+        int[] firstCodingLeft = Arrays.copyOfRange(firstCoding, 0, cutPoint);
+        int[] firstCodingRight = Arrays.copyOfRange(firstCoding, cutPoint, minLength);
+        int[] secondCodingLeft = Arrays.copyOfRange(secondCoding, 0, cutPoint);
+        int[] secondCodingRight = Arrays.copyOfRange(secondCoding, cutPoint, minLength);
+
+        ChessboardIndividual offspring1 = (ChessboardIndividual) pairing.firstIndividual.clone();
+        offspring1.setEncoding(Stream.of(firstCodingLeft, secondCodingRight)
+                .flatMapToInt(Arrays::stream)
+                .toArray()
+        );
+        ChessboardIndividual offspring2 = (ChessboardIndividual) pairing.secondIndividual.clone();
+        offspring2.setEncoding(Stream.of(secondCodingLeft, firstCodingRight)
+                .flatMapToInt(Arrays::stream)
+                .toArray()
+        );
+        return new Pairing(offspring1, offspring2);
     }
 }
